2cd5f30e77574f506729ee00436f5e3d29684c0d
Mors <alongdead@gmail.com>
2026-02-27 19:01:04 +0200

    Initial commit for login page project

---
+++ main.js
@@ -0,0 +1,41 @@
+let form = document.getElementById("loginForm");
+
+
+const dataBase = [
+    {email: "adming@gmail.com",
+    password: "123456"},
+    {
+    email: "mors@gmail.com",
+    password: "123456"
+    },{
+    email: "chihiro@gmail.com",
+    password: "123456"
+    }       
+]
+
+
+form.addEventListener("submit",function(event){
+    event.preventDefault();
+
+    let found = false;
+    const email = document.getElementById("femail").value;
+    const password = document.getElementById("fpassword").value;
+
+
+    const errorMessage = document.getElementById("errorMessage");
+
+
+   for(let i = 0;i < dataBase.length; i++){
+        if(email === dataBase[i].email && password === dataBase[i].password){
+            console.log("Login successfull");
+            found = true;
+            break;
+        }
+   }
+
+   if(!found){
+    errorMessage.innerText ="Invalid email or password";
+   } else {
+    errorMessage.innerText = "";
+   }
+})
